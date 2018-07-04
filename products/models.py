999d4175ce7afd876cb740206e24e4283beefbec
Shubham Goswami <sgshubham98@gmail.com>
2018-07-04 21:49:36 +0530

    Added various functionalities and apps

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=120)
+	pub_date = models.DateTimeField()
+	url = models.TextField()
+	body = models.TextField()
+	thumbnail = models.ImageField(upload_to='images/')
+	image = models.ImageField(upload_to='images/')
+	votes_total = models.IntegerField(default = 0)
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:60]
+
+	def pub_date_good(self):
+			return self.pub_date.strftime('%b %e %Y')
